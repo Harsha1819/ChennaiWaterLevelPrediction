4855f27d2e02052d4b854295f564f2280457de01
Harsha1819 <130477588+Harsha1819@users.noreply.github.com>
2023-04-11 20:21:37 +0530

    Add files via upload

---
+++ Harsha.py
@@ -0,0 +1,91 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import io
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from datetime import datetime
+
+# Load the data
+df = pd.read_excel(r"C:\Users\harsh\RainfallandWaterLevel.xlsx")
+
+# Define the features and target
+X = df[['POONDI', 'CHOLAVARAM', 'REDHILLS', 'CHEMBARAMBAKKAM']].values
+y = df['Date'].values
+
+# Fit the model
+model = LinearRegression()
+model.fit(X, y)
+
+
+Model = pickle.load(open(r'C:\Users\harsh/Model.pkl', 'rb'))
+
+
+def predict():
+
+    st.title("Total Water Level Prediction")
+    
+date_str = st.text_input("Enter date (yyyy-mm-dd):", "")
+
+# Convert date input to datetime object
+date = datetime.strptime(date_str, "%Y-%m-%d") if date_str else None
+
+# If date is provided, predict total water level
+if date:
+    # Convert date to numerical features for prediction
+    year = date.year
+    month = date.month
+    day = date.day
+
+    # Call the machine learning model to predict the total water level
+    total_water_level = model.predict([[year, month, day]])
+
+    # Display the predicted total water level
+    st.success(f"Predicted total water level for {date_str}: {total_water_level[0]}")
+
+
+plt.rcParams["figure.figsize"] = [12, 6]
+plt.rcParams["figure.autolayout"] = True
+
+
+# Define the function for making predictions
+def predict_water_level(pondi, cholavaram, redhills, chembarambakkam):
+    water_level = model.predict([[pondi, cholavaram, redhills, chembarambakkam]])
+    return water_level[0]
+
+def main():
+
+
+    menu = ['Home', 'Chennai Water Level','Predict']
+    choice = st.sidebar.selectbox('Select an option', menu)
+
+    if choice == 'Home':
+        st.header('Welcome to the Rainfall and Water Level Prediction app!')
+        st.write('This app uses a trained model to predict the water level for a given date on the historical data.')
+
+
+    elif choice == 'Predict':
+        st.header('Water Level Prediction')
+        predict()
+       
+    elif choice == "Chennai Water Level":
+        # Create the Streamlit app
+        st.title('Chennai Water Level Prediction')
+
+        # Add the inputs for each reservoir
+        pondi_level = st.slider('Pondi Reservoir Level', 0, 5000, 1000)
+        cholavaram_level = st.slider('Cholavaram Reservoir Level', 0, 5000, 1000)
+        redhills_level = st.slider('Redhills Reservoir Level', 0, 5000, 1000)
+        chembarambakkam_level = st.slider('Chembarambakkam Reservoir Level', 0, 5000, 1000)
+
+        # Make the prediction and display the result
+        water_level = predict_water_level(pondi_level, cholavaram_level, redhills_level, chembarambakkam_level)
+        st.write('The predicted water level for Chennai is:', water_level)
+
+
+
+if __name__ == '__main__':
+    main()
