81abb4b245d4912c9801c9942d8e141f7f3b4564
Harsha1819 <130477588+Harsha1819@users.noreply.github.com>
2023-04-22 19:48:57 +0530

    Update Harsha.py

--- Harsha.py
+++ Harsha.py
@@ -52,7 +52,7 @@ def main():
 
         # Make the prediction and display the result
         water_level = predict_water_level(pondi_level, cholavaram_level, redhills_level, chembarambakkam_level)
-        st.write('The predicted water level for Chennai is : ', f" water_level ml")
+        st.write('The predicted water level for Chennai is : ', water_level ml")
 
 if __name__ == '__main__':
     main()
